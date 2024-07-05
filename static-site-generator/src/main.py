4103ebd77e5219168b35ee751522bd7f7b9eb00f
purplesunk <84647408+purplesunk@users.noreply.github.com>
2024-07-05 11:14:25 -0300

    Add split nodes by delimiter and split images and links

--- static-site-generator/src/main.py
+++ static-site-generator/src/main.py
@@ -1,3 +1,5 @@
+import re
+
 from textnode import TextNode
 from htmlnode import HTMLNode 
 from leafnode import LeafNode 
@@ -31,39 +33,78 @@ def text_node_to_html_node(text_node):
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for node in old_nodes:
-        if isinstance(node, TextNode):
-            words = node.text.split()
-            found_delimiter_start = False
-            found_delimiter_end = False
-            new_node = []
+        if node.text_type == text_type_text:
+            splited_node = node.text.split(delimiter)
 
-            for word in words:
-                if found_delimiter_start:
-                    if word.endswith(delimiter):
-                        found_delimiter_end = True
-                        new_node.append(word.rstrip(delimiter))
-                        continue
-                    new_node.append(word)
+            if len(splited_node) % 2 == 0:
+                raise Exception(f"Invalid Markdown Syntax: Could not find matching closing delimiter in {node} with {delimiter}.")
 
-                if word.startswith(delimiter):
-                    found_delimiter_start = True
-                    new_node.append(word.lstrip(delimiter))
-
-            if (found_delimiter_start 
-                and not found_delimiter_end):
-                raise Exception("Could not find matching closing delimiter in {node} with {delimiter}.")
-            new_nodes.append(TextNode(" ".join(new_node), text_type))
+            for i in range(0, len(splited_node)):
+                if splited_node[i] and i % 2 != 0: 
+                    new_nodes.append(TextNode(splited_node[i], text_type))
+                elif splited_node[i]:
+                    new_nodes.append(TextNode(splited_node[i], text_type_text))
         else:
             new_nodes.append(node)
+    return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        images = extract_markdown_images(node.text)
+
+        if not images:
+            new_nodes.append(node)
+            continue
+
+        original_text = node.text
+        
+        new_node_list = []
+        for image_tup in images:
+            splited_text = original_text.split(f"![{image_tup[0]}]({image_tup[1]})", 1)
+            if splited_text[0]:
+                new_node_list.append(TextNode(splited_text[0], node.text_type))
+            new_node_list.append(TextNode(image_tup[0], text_type_image, image_tup[1]))
+            original_text = splited_text[1];
+
+        new_nodes.extend(new_node_list)
+
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        links = extract_markdown_links(node.text)
+
+        if not links:
+            new_nodes.append(node)
+            continue
+
+        original_text = node.text
+        
+        new_node_list = []
+        for link_tup in links:
+            splited_text = original_text.split(f"[{link_tup[0]}]({link_tup[1]})", 1)
+            if splited_text[0]:
+                new_node_list.append(TextNode(splited_text[0], node.text_type))
+            new_node_list.append(TextNode(link_tup[0], text_type_link, link_tup[1]))
+            original_text = splited_text[1];
+
+        new_nodes.extend(new_node_list)
+
+    return new_nodes
 
 
 def main():
-    node = TextNode("This is text with a `code block` word", text_type_text)
-    new_nodes = split_nodes_delimiter([node], "`", text_type_code)
-    print(new_nodes)
-
-    tn = TextNode("This is a text node", "bold", "https://www.boot.dev") 
-    print(tn)
+    print("hello")
 
 
 main()
